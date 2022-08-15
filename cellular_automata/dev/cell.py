2ec425a224e65b11958f3ff76524126f90c89cbd
JamesdeLeon <jdeleonphys@gmail.com>
2022-08-15 01:07:36 -0500

    added documentation and adjusted parameter readout mechanism;

--- cellular_automata/dev/cell.py
+++ cellular_automata/dev/cell.py
@@ -1,22 +1,58 @@
 # CELL PIXEL CLASS
 
+import json
 import random
-from hyperparameters import Hyperparameters as h
 
 class Cell:
 	'''
+	DESCRIPTION:
+		- classifies a cell on a grid of cells
+
+	METHODS:
+		- ability to preset certain parameters based on
+			the parameters json file
+		- ability to define an initial state for the cell (0, 1)
+		- ability to define a color for the cell based on its state
+
+	ATTRIBUTES:
+		- init boolean, which determines whether the cell is 
+			first initialized or iteratively updated
+		- parameter dictionary
+		- state boolean (0, 1)
+		- color coordinate tuple (R, G, B)
+		- a counter for the cell's number of alive neighbors
 	'''
 	def __init__(self, init: bool) -> None: 
 		self.init = init
+		self.params = self.define_params()
 		self.state = self.define_state()
 		self.color = self.define_color()
 		self.neighbors = 0
 
+	def define_params(self) -> dict:
+		'''
+		- reads parameter json file and loads dict data
+		'''
+		with open("parameters.json", 'r') as file:
+			data = json.load(file)
+		file.close()
+		return data
+
 	def define_state(self) -> int:
+		'''
+		- randomly assigns an initial state for the cell
+		- if state == 1: "alive"
+		- if state == 0: "dead"
+		'''
 		return random.choice([0, 1]) if self.init else 0
 
 	def define_color(self) -> tuple:
-		return h.WHITE if self.state == 1 else h.BLACK
+		'''
+		- defines a color based on the cell's state
+		- white if state == 1
+		- black if state == 0
+		'''
+		return (255, 255, 255) if self.state == 1 else (0, 0, 0)
 
 
 if __name__ == "__main__":
--- cellular_automata/dev/game.py
+++ cellular_automata/dev/game.py
@@ -1,40 +1,74 @@
 # AUTOMATA PYGAME CLASS
 
+import json
+import random
 import pygame
 from pygame.locals import QUIT, MOUSEBUTTONDOWN
-import random
 from grid import Grid
-from hyperparameters import Hyperparameters as h
 
 class Game:
 	'''
+	DESCRIPTION:
+		- visualizes the system with the pygame library
+
+	METHODS:
+		- ability to read out preset parameters
+		- ability to draw a grid object every frame of the animation
+		- ability to continuously run the animation unless specified
+			otherwise
+
+	ATTRIBUTES:
+		- parameter dictionary
+		- pygame initializers
+		- initial and iteravely continuous grid object
+		- cell width based on parameters
+		- cell height based on parameters
 	'''
 	def __init__(self) -> None:
+		self.params = self.define_params()
 		pygame.init()
 		pygame.display.set_caption("John Conway's Game of Life")
-		self.screen = pygame.display.set_mode((h.WINDOW_WIDTH, h.WINDOW_HEIGHT))
+		self.screen = pygame.display.set_mode((self.params["Window Width"], self.params["Window Height"]))
 		self.clock = pygame.time.Clock()
 		self.grid = Grid()
+		self.cell_width = int(self.params["Window Width"] / self.params["DIM"])
+		self.cell_height = int(self.params["Window Height"] / self.params["DIM"])
 		self.running = True
 
+	def define_params(self) -> dict:
+		'''
+		- reads parameter json file and loads dict data
+		'''
+		with open("parameters.json", 'r') as file:
+			data = json.load(file)
+		file.close()
+		return data
+
 	def draw_grid(self) -> None:
-		for row in range(0, h.DIM):
-			for col in range(0, h.DIM):
-				color = h.BLACK
-				if row in [0, h.DIM - 1] or col in [0, h.DIM - 1]: 
+		'''
+		- assigns states to each cell in the grid object
+		'''
+		for row in range(0, self.params["DIM"]):
+			for col in range(0, self.params["DIM"]):
+				color = (0, 0, 0)
+				if row in [0, self.params["DIM"] - 1] or col in [0, self.params["DIM"] - 1]: 
 					self.grid.grid[row][col].state = 0
-				if self.grid.grid[row][col].state == 1: color = h.WHITE
+				if self.grid.grid[row][col].state == 1: color = (255, 255, 255)
 				pygame.draw.rect(surface = self.screen,
 								 color = color, 
-								 rect = [h.CELL_WIDTH * col, 
-								 		 h.CELL_HEIGHT * row, 
-								 		 h.CELL_WIDTH, 
-								 		 h.CELL_HEIGHT]
+								 rect = [self.cell_width * col, 
+								 		 self.cell_height * row, 
+								 		 self.cell_width, 
+								 		 self.cell_height]
 				)
 
 	def run(self) -> None:
+		'''
+		- continously runs the animation
+		- updates the grid object with each frame of the animation
+		'''
 		while self.running:
-			self.screen.fill(h.WHITE)
+			self.screen.fill((255, 255, 255))
 			for event in pygame.event.get():
 				if event.type == QUIT: self.running = False
 				elif event.type == MOUSEBUTTONDOWN: self.__init__()
