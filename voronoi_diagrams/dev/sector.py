71c66cbd134ddb8ac5c4ddc24d3c17f77dbf7818
JamesdeLeon <jdeleonphys@gmail.com>
2022-08-23 22:45:19 -0500

    add sector script; work will have to be done to determine which of these scripts is redundant

---
+++ voronoi_diagrams/dev/sector.py
@@ -0,0 +1,22 @@
+# SECTOR CLASS
+
+class Sector:
+	'''
+	DESCRIPTION:
+		- a section of the game window that subdivides
+			if there is more than one site within it
+		- checks each of its four corners to determine if
+			a subdivision is needed
+
+	METHODS:
+		-
+
+	ATTRIBUTES:
+		-
+	'''
+	def __init__(self) -> None:
+		pass
+
+
+if __name__ == "__main__":
+	pass
